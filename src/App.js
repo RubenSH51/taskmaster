5e4270550dba4797498b22ba1265fe4bb7375d14
RubenSH51 <rubencuello51@gmail.com>
2023-02-26 18:27:30 -0300

    Primer clase del módulo 3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 //import React from 'react' // rafc is the shortcut // Apparently we don't need the first line 
-import { Header } from "./components/Header"                                                    // if you're using react over v16 version
-import { Footer } from "./components/Footer"  
+
 
 export const App = () => {
   return (
-    <>
-      <Header />
-      <div>
-        <h1>Rubén</h1>
+
+      <div className="App">
+        <h1>Hello World!</h1>
       </div>
-      <Footer />
-    </>
   )
 }

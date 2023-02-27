a7d4f3d5fbc457d2c5d5db314b45521fe7409a1c
RubenSH51 <rubencuello51@gmail.com>
2023-02-26 22:39:51 -0300

    Tasks list base

--- src/App.js
+++ src/App.js
@@ -5,78 +5,41 @@ import { useState } from "react";
 
 export const App = () => {
 
-  //let count = 0;
+  const [tasks, setTasks] = useState([
+    {id: 1432,
+    name: "Watch class",
+    completed: false},
+    {id: 2453,
+    name: "Become a pro",
+    completed: false},
+    {id: 3975,
+    name: "Make courses",
+    completed: false}
 
-  const [count, setCount] = useState(0)
 
-  function handleAdd()
+  ])
+
+  function deleteTask(id)
   {
-    //count++;  
-    if (count < 100000)
-    {
-      setCount(count + 1)
-    }
-    else
-    {
-      return 
-    }
-    
-  }
+    // console.log(id)
 
-  function handleSub()
-  {
-    if (count > -5000)
-    {
-      setCount(count - 1)
-    }
-    else
-    {
-      return 
-    }
-  }
+    setTasks(tasks.filter(task => id !== task.id))
 
-  function reset()
-  {
-    setCount(0)
-  }
-
-
-  function esEntero(num)
-  {
-    return (num % 1===0)
-  }
-
-  function formatoNumero(numero)
-  {
-    if (esEntero(numero*1.8+32))
-    {
-      return (numero*1.8+32)
-    }
-    else
-    {
-      return (numero*1.8+32).toFixed(2)
-    }
   }
 
 
   return (
 
       <div className="App">
-          <div className="box">
-            <p>{count} °C</p>
-            <div className="botonera">
-
-              <div className="upperDiv">
-                <button onClick={handleSub} className="sub">- 😢</button>
-                <button onClick={handleAdd} className="add">+ 😊</button>
-              </div>
-              <div className="lowererDiv">
-                <button className="reset" onClick={reset}>Reset 😮</button>
-              </div>
-              
-            </div>
-            <p>{formatoNumero(count)} °F</p>
-          </div>
+        <h1>Tasks list</h1>
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.name}>
+              <span>{task.id} - {task.name}</span>
+              <button onClick={() => deleteTask(task.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
       </div>
   )
 }

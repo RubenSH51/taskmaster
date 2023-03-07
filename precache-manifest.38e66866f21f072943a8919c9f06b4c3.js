0efa5dd94735ede8323ab8507a4e1f920f62d1e0
RubenSH51 <rubencuello51@gmail.com>
2023-03-07 18:24:38 -0300

    Updates

---
+++ precache-manifest.38e66866f21f072943a8919c9f06b4c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "adb37e530e05389d2f03",
+    "url": "/taskmaster/static/css/main.e52854d5.chunk.css"
+  },
+  {
+    "revision": "adb37e530e05389d2f03",
+    "url": "/taskmaster/static/js/main.adb37e53.chunk.js"
+  },
+  {
+    "revision": "ed8ca8b358d8803c0033",
+    "url": "/taskmaster/static/js/1.ed8ca8b3.chunk.js"
+  },
+  {
+    "revision": "2010b95fa07689428f85",
+    "url": "/taskmaster/static/js/runtime~main.2010b95f.js"
+  },
+  {
+    "revision": "91ac5f0ff950d6e20f40d5e801038d60",
+    "url": "/taskmaster/index.html"
+  }
+];
